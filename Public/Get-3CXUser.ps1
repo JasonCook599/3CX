904cd2f88395f0bb71f51045ff688813d43ef3c0
Jason Cook <JasonCook599@users.noreply.github.com>
2024-09-26 15:22:50 -0400

    Add support for a single user

--- Public/Get-3CXUser.ps1
+++ Public/Get-3CXUser.ps1
@@ -9,22 +9,49 @@
         powershell object containing the 3CX response
 #>
 function Get-3CXUser {
-  [CmdletBinding()]
+  [CmdletBinding(DefaultParameterSetName = "Default")]
   param(
+    [Parameter(ParameterSetName = "Default")]
     $Filter = "not startsWith(Number,'HD')",
+
+    [Parameter(ParameterSetName = "Default")]
     $Order = "Number",
+
+    [Parameter(ParameterSetName = "Default")]
+    [Parameter(ParameterSetName = "Single")]
     $Select = "IsRegistered,CurrentProfileName,DisplayName,Id,EmailAddress,Number,Tags,Require2FA",
+
+    [Parameter(ParameterSetName = "Default")]
+    [Parameter(ParameterSetName = "Single")]
     $Expand = 'Groups($select=GroupId,Name,Rights;$filter=not startsWith(Name,''___FAVORITES___'');$expand=Rights($select=RoleName)),Phones($select=MacAddress,Name,Settings($select=IsSBC,ProvisionType))',
-    [int]$Limit
+
+    [Parameter(ParameterSetName = "Default")]
+    [int]$Limit,
+
+    [Parameter(ParameterSetName = "Single", Mandatory = $True)]
+    [int]$ID
   )
-  $params = @{
-    Endpoint    = '/xapi/v1/Users'
-    Paginate    = $true
-    PageFilter  = $Filter
-    PageOrderBy = $Order
-    PageSelect  = $Select
-    PageExpand  = $Expand
-    Limit       = $Limit
+
+  switch ($PSCmdlet.ParameterSetName) {
+    "Default" {
+      $params = @{
+        Endpoint    = '/xapi/v1/Users'
+        Paginate    = $true
+        PageFilter  = $Filter
+        PageOrderBy = $Order
+        PageSelect  = $Select
+        PageExpand  = $Expand
+        Limit       = $Limit
+      }
+    }
+    "Single" {
+      $params = @{
+        Endpoint   = "/xapi/v1/Users"
+        ID         = $ID
+        PageSelect = $Select
+        PageExpand = $Expand
+      }
+    }
   }
   return Get-3CXResult @params
 }
